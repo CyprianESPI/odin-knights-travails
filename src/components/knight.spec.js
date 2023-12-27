5ab64e358bee58af05d13c09b0537870d824651f
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-27 14:27:22 +0100

    Fix Jest import error
    Install babel and add .babelrc file
    npm install --save-dev @babel/core @babel/preset-env babel-jest

--- src/components/knight.spec.js
+++ src/components/knight.spec.js
@@ -1,6 +1,6 @@
-const { default: Knight } = require('./knight');
+import Knight from './knight';
 
-describe('ll', () => {
+describe('Knight', () => {
     test('moves test 1', () => {
         //If it should pass with deep equality, replace "toBe" with "toStrictEqual"
         expect(Knight.moves([0, 0], [3, 3])).toStrictEqual([[0, 0], [2, 1], [3, 3]]);
