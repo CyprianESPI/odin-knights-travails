ecc09e2040fd17848f1c18cf4028d3e6d279d1b7
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-27 15:08:34 +0100

    Add possible knight's move

--- src/components/knight.js
+++ src/components/knight.js
@@ -1,7 +1,16 @@
 class Knight {
+    // origin/destination is an array of numbers [x, y]
     static moves(origin, destination) {
         console.log(origin, destination);
         return true;
     }
+
+    // List all possible moves x,y moves
+    static POSSIBLE_MOVES = [
+        [1, 2], [2, 1],
+        [2, -1], [1, -2],
+        [-1, -2], [-2, -1],
+        [-2, 1], [-1, 2],
+    ];
 }
 export default Knight;
